e1acea85b4556200c558c930c56395438dfc56e1
Simon Lagos <simon.lagos.sallhed@gmail.com>
2020-12-08 10:20:04 +0100

    Add types

--- index.ts
+++ index.ts
@@ -4,22 +4,40 @@ const MAX_ANGLE = 10
 const MAX_OFFSET = 30
 const TRAUMA_REDUCTION_PER_UPDATE = 0.03
 
+type Options = {
+  maxAngle?: number
+  maxOffsetX?: number
+  maxOffsetY?: number
+  traumaReductionPerUpdate?: number
+}
+
+type UpdateResult = {
+  angle: number
+  offsetX: number
+  offsetY: number
+}
+
+type QuakeInstance = {
+  add: (trauma: number) => void
+  update: (time: number) => UpdateResult
+}
+
 const quake = ({
   maxAngle = MAX_ANGLE,
   maxOffsetX = MAX_OFFSET,
   maxOffsetY = MAX_OFFSET,
   traumaReductionPerUpdate = TRAUMA_REDUCTION_PER_UPDATE,
-} = {}) => {
+}: Options = {}): QuakeInstance => {
   let currentTrauma = 0
   const anglePerlin = new Perlin()
   const offsetXPerlin = new Perlin()
   const offsetYPerlin = new Perlin()
 
   return {
-    add: (trauma) => {
+    add: (trauma: number): void => {
       currentTrauma = Math.min(currentTrauma + trauma, 1)
     },
-    update: (time) => {
+    update: (time: number): UpdateResult => {
       const shake = currentTrauma ** 2
       const angle = maxAngle * shake * anglePerlin.noise(time, 1)
       const offsetX = maxOffsetX * shake * offsetXPerlin.noise(time, 1)
